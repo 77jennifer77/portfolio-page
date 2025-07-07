eb1c6296601b938a848988edfee6e3d22f3e3862
Jennifer Chiang <77jennifer77@gmail.com>
2025-07-06 18:07:40 -0700

    Create basic template pages

---
+++ src/pages/Contact.tsx
@@ -0,0 +1,7 @@
+export default function Contact() {
+  return (
+    <div className="text-center text-2xl font-bold text-blue-600">
+      Contact Page
+    </div>
+  );
+}
---
+++ src/pages/Resume.tsx
@@ -0,0 +1,7 @@
+export default function Resume() {
+  return (
+    <div className="text-center text-2xl font-bold text-blue-600">
+      Resume Page
+    </div>
+  );
+}
