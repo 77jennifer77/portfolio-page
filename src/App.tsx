7c2893791493dbcb760ab3167c14be46830fa40c
Jennifer Chiang <77jennifer77@gmail.com>
2025-07-13 13:22:34 -0700

    Add react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import pageRoutes from "./routes/pageRoutes";
+import type { PageRoute } from "./routes/pageRoutes";
+// If you get a module not found error, try changing the import to './routes/pageRoute.tsx'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <Router>
+      <NavBar />
+      <div className="min-h-screen bg-gray-100">
+        <Routes>
+          {pageRoutes.map((route: PageRoute) => (
+            <Route key={route.name} path={route.path} element={route.element} />
+          ))}
+        </Routes>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </Router>
+  );
 }
 
-export default App
+export default App;
