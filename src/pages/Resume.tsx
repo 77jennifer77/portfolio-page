36630caff6c208cb09876e3ae61be3569a6adfff
Jennifer Chiang <77jennifer77@gmail.com>
2025-07-20 20:54:31 -0700

    Add resume as iframe

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -2,6 +2,7 @@ export default function Resume() {
   return (
     <div className="text-center text-2xl font-bold text-blue-600">
       Resume Page
+      <iframe src="src/assets/Jennifer_Chiang_Resume.pdf" width="600" height="800" title="Resume"></iframe>
     </div>
   );
 }
