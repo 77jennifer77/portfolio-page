32b15ee53d94dcc43c3affe9482280222cddecfa
Jennifer Chiang <77jennifer77@gmail.com>
2025-08-23 10:50:50 -0700

    Update routing for SPA

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,27 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import NavBar from "./components/NavBar";
-import pageRoutes from "./routes/pageRoutes";
-import type { PageRoute } from "./routes/pageRoutes";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import NavBar from "./components/NavBar";
+// import pageRoutes from "./routes/pageRoutes";
+// import type { PageRoute } from "./routes/pageRoutes";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
 // If you get a module not found error, try changing the import to './routes/pageRoute.tsx'
 
 function App() {
   return (
-    <Router>
-      <NavBar />
-      <div className="min-h-screen bg-black text-white">
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      {/* <NavBar />
+      <>
         <Routes>
           {pageRoutes.map((route: PageRoute) => (
             <Route key={route.name} path={route.path} element={route.element} />
           ))}
         </Routes>
-      </div>
-    </Router>
+      </> */}
+    </BrowserRouter>
   );
 }
 
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -13,14 +13,14 @@ export default function Navbar() {
   return (
     <nav className="bg-white shadow-md sticky top-0 z-50">
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-        <div className="flex items-center justify-between h-16">
+        <div className="flex items-center justify-between items-center h-16">
           {/* Logo / Brand */}
-          <Link to="/" className="text-xl font-bold text-blue-600">
+          <Link to="/" className="text-xl font-bold">
             {data.name}'s Portfolio
           </Link>
 
           {/* Desktop Nav */}
-          <div className="md:flex space-x-6">
+          <div className="hidden md:flex ml-auto space-x-6">
             {navLinks.map((link: PageRoute) =>
                 <NavLink
                   key={link.name}
