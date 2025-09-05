c3df7a53a3326a47235d165241effd489240087b
Jennifer Chiang <77jennifer77@gmail.com>
2025-09-05 11:12:38 -0700

    Add styling to home class for responsive design

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="min-h-screen">
+    <div className="min-h-screen container mx-auto px-4">
         {/* Theme Toggle*/}
         {/* Background Effects */}
         {/* NavBar */}
