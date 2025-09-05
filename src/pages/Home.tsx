84b68ab741db3f2bb34bb8b2bd8ddb52b3954bbc
Jennifer Chiang <77jennifer77@gmail.com>
2025-09-05 10:25:27 -0700

    Add theme and modify home to fit multiple screens

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,11 @@
 export default function Home() {
   return (
-    <div id="home" className="text-center text-2xl font-bold text-blue-600">
-      Home Page
+    <div className="min-h-screen">
+        {/* Theme Toggle*/}
+        {/* Background Effects */}
+        {/* NavBar */}
+        {/* Main Content */}
+        {/* Footer */}
     </div>
   );
 }
