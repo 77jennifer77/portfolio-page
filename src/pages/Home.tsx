843ea7d5b023bc2e8e977d5be5356b0e30abd3a9
Jennifer Chiang <77jennifer77@gmail.com>
2025-09-05 12:36:58 -0700

    Add ToggleTheme to Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,16 @@
+import { ThemeToggle } from "../components/ThemeToggle";
+
 export default function Home() {
   return (
-    <div className="min-h-screen container mx-auto px-4">
+    <div className="min-h-screen container mx-auto px-4 bg-background text-foreground overflow-x-hidden">
         {/* Theme Toggle*/}
+        <ThemeToggle />
         {/* Background Effects */}
+    
         {/* NavBar */}
+    
         {/* Main Content */}
+    
         {/* Footer */}
     </div>
   );
